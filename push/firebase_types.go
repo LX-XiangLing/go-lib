8af8a61a1117e2b13f27ad43e2ea65da93dae706
lixiang <lixiang2014@xiaochuankeji.cn>
2022-07-11 15:26:40 +0800

    ADD: firebase push

--- push/firebase_types.go
+++ push/firebase_types.go
@@ -5,3 +5,6 @@ import "firebase.google.com/go/v4/messaging"
 type MulticastMessage = messaging.MulticastMessage
 type BatchResponse = messaging.BatchResponse
 type Notification = messaging.Notification
+type AndroidConfig = messaging.AndroidConfig
+type WebpushConfig = messaging.WebpushConfig
+type APNSConfig = messaging.APNSConfig
